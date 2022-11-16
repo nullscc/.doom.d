32b854188df2745586ab64bd0b38d6f53994a7b4
hoo null <hoo@hoos-MacBook-Air.local>
2022-11-16 18:13:12 +0800

    first init

---
+++ config.el
@@ -0,0 +1,268 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-gruvbox)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Documents/orgroam/") ;; TOCONF
+
+(use-package! rainbow-delimiters
+    :hook (prog-mode . rainbow-delimiters-mode))
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package! lsp-mode
+  :hook (lsp-mode . efs/lsp-mode-setup))
+
+(global-set-key "\C-s" 'swiper)
+
+;; (setq projectile-indexing-method 'native)
+(setq projectile-globally-ignored-files
+      (append '(
+        ".DS_Store"
+        "*.wav"
+        "*.npy"
+        "*.tar.gz"
+        "*.tgz"
+        "*.zip"
+        )
+          projectile-globally-ignored-files))
+
+(exec-path-from-shell-initialize)
+
+(use-package! org-roam
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol)
+  :custom
+  (org-roam-completion-everywhere t)
+  (org-roam-directory "~/Documents/orgroam") ;; TOCONF
+  (org-roam-capture-templates
+   '(("d" "default" plain "%?" :target
+      (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)))
+  :bind (:map org-mode-map
+         ("C-M-i"    . completion-at-point)))
+
+(use-package! websocket
+    :after org-roam)
+
+(use-package! ivy-bibtex
+  :config
+  (setq bibtex-completion-bibliography "~/Documents/material/paper.bib" ;; TOCONF
+        bibtex-completion-notes-path "~/Documents/orgroam/paper.org" ;; TOCONF
+        bibtex-completion-display-formats '((t . " ${title:*} ${year:4} ${domain:13} ${pubon:11} ${dataset:50} ${=has-pdf=:1}${=has-note=:1}"))
+        bibtex-completion-notes-template-one-file "\n* (${year}) - ${title} :${domain}:\n  :PROPERTIES:\n  :Custom_ID: ${=key=}\n  :NOTER_DOCUMENT: ${pdf}\n  :END:\n** Overview\n"
+        bibtex-completion-pdf-field "pdf"
+        bibtex-completion-additional-search-fields '(domain dataset pubon))
+  :custom
+  (ivy-height 25)
+  :bind
+  ("C-c n B" . ivy-bibtex))
+
+(use-package! org-noter
+  :custom
+  (org-noter-default-notes-file-names '("/Users/hoo/Documents/orgroam/paper.org")))
+
+(add-hook 'org-mode-hook 'valign-mode)
+
+;; allow for export=>beamer by placing
+;; #+LaTeX_CLASS: beamer in org files
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+(add-to-list 'org-export-latex-classes
+  ;; beamer class, for presentations
+  '("beamer"
+     "\\documentclass[11pt]{beamer}\n
+      \\mode<{{{beamermode}}}>\n
+      \\usetheme{{{{beamertheme}}}}\n
+      \\usecolortheme{{{{beamercolortheme}}}}\n
+      \\beamertemplateballitem\n
+      \\setbeameroption{show notes}
+      \\usepackage[utf8]{inputenc}\n
+      \\usepackage[T1]{fontenc}\n
+      \\usepackage{hyperref}\n
+      \\usepackage{color}
+      \\usepackage{listings}
+      \\lstset{numbers=none,language=[ISO]C++,tabsize=4,
+  frame=single,
+  basicstyle=\\small,
+  showspaces=false,showstringspaces=false,
+  showtabs=false,
+  keywordstyle=\\color{blue}\\bfseries,
+  commentstyle=\\color{red},
+  }\n
+      \\usepackage{verbatim}\n
+      \\institute{{{{beamerinstitute}}}}\n
+       \\subject{{{{beamersubject}}}}\n"
+
+     ("\\section{%s}" . "\\section*{%s}")
+
+     ("\\begin{frame}[fragile]\\frametitle{%s}"
+       "\\end{frame}"
+       "\\begin{frame}[fragile]\\frametitle{%s}"
+       "\\end{frame}")))
+
+
+;;(use-package! bibtex
+;;  :config
+;;  ;; Change fields and format
+;;  (setq bibtex-user-optional-fields
+;;        '(("keywords" "Keywords to describe the entry" "")
+;;          ("file" "Link to document file." ":"))
+;;        bibtex-align-at-equal-sign t
+;;        bib-files-directory (directory-files
+;;                             (concat (getenv "HOME") "/Bib") t
+;;                             "^[A-Z|a-z].+.bib$")
+;;        pdf-files-directory (concat (getenv "HOME") "/Pdf")))
+;;
+;;
+;;(use-package! helm-bibtex
+;;  :config
+;;  (setq bibtex-completion-bibliography bib-files-directory
+;;        bibtex-completion-library-path pdf-files-directory
+;;        bibtex-completion-pdf-field "File"
+;;        bibtex-completion-notes-path org-directory
+;;        bibtex-completion-additional-search-fields '(keywords))
+;;  :bind
+;;  (("C-c n B" . helm-bibtex)))
+;;
+;;;;Spell checking (requires the ispell software)
+;;(add-hook 'bibtex-mode-hook 'flyspell-mode)
+;;
+;;(use-package! org-roam-bibtex
+;;  :after (org-roam helm-bibtex)
+;;  :bind (:map org-mode-map ("C-c n b" . orb-note-actions))
+;;  :config
+;;  (require 'org-ref))
+;;
+;;(org-roam-bibtex-mode)
+;;(map! :after helm
+;;           :map helm-map
+;;           "TAB"      #'helm-select-action
+;;            [tab]      #'helm-select-action
+;;            "C-z"      #'helm-execute-persistent-action)
+;;;; helm-bibtex related stuff
+;;(after! helm
+;;  (use-package! helm-bibtex
+;;    :custom
+;;    ;; In the lines below I point helm-bibtex to my default library file.
+;;    (bibtex-completion-bibliography '("/Users/hoo/Desktop/hello.bib"))
+;;    ;; The line below tells helm-bibtex to find the path to the pdf
+;;    ;; in the "file" field in the .bib file.
+;;    (bibtex-completion-pdf-field "file")
+;;    :hook (Tex . (lambda () (define-key Tex-mode-map "\C-ch" 'helm-bibtex))))
+;;  ;; I also like to be able to view my library from anywhere in emacs, for example if I want to read a paper.
+;;  ;; I added the keybind below for that.
+;;  (map! :leader
+;;        :desc "Open literature database"
+;;        "o l" #'helm-bibtex)
+;;  ;; And I added the keybinds below to make the helm-menu behave a bit like the other menus in emacs behave with evil-mode.
+;;  ;; Basically, the keybinds below make sure I can scroll through my list of references with C-j and C-k.
+;;  (map! :map helm-map
+;;        "C-j" #'helm-next-line
+;;        "C-k" #'helm-previous-line)
+;;)
+;;
+;; ;; Set up org-ref stuff
+;; (use-package! org-ref
+;;    :custom
+;;    ;; Again, we can set the default library
+;;    (org-ref-default-bibliography "/ssdhome/xzw521/hello.bib"))
+;;    ;; The default citation type of org-ref is cite:, but I use citep: much more often
+;;    ;; I therefore changed the default type to the latter.
+;;    ;; (org-ref-default-citation-link "citep")
+
+ ;; The function below allows me to consult the pdf of the citation I currently have my cursor on.
+;; (defun my/org-ref-open-pdf-at-point ()
+;;  "Open the pdf for bibtex key under point if it exists."
+;;  (interactive)
+;;  (let* ((results (org-ref-get-bibtex-key-and-file))
+;;         (key (car results))
+;;         (pdf-file (funcall org-ref-get-pdf-filename-function key)))
+;;    (if (file-exists-p pdf-file)
+;;        (find-file pdf-file)
+;;      (message "No PDF found for %s" key))))
+;;
+;; (setq org-ref-completion-library 'org-ref-ivy-cite
+;;       org-export-latex-format-toc-function 'org-export-latex-no-toc
+;;       org-ref-get-pdf-filename-function
+;;       (lambda (key) (car (bibtex-completion-find-pdf key)))
+;;       ;; See the function I defined above.
+;;       org-ref-open-pdf-function 'my/org-ref-open-pdf-at-point
+;;       ;; For pdf export engines.
+;;       org-latex-pdf-process (list "latexmk -pdflatex='%latex -shell-escape -interaction nonstopmode' -pdf -bibtex -f -output-directory=%o %f")
+;;       ;; I use orb to link org-ref, helm-bibtex and org-noter together (see below for more on org-noter and orb).
+;;       org-ref-notes-function 'orb-edit-notes)
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
+ '(org-sparse-tree-open-archived-trees nil)
+ '(pdf-view-continuous t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
